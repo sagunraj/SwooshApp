ba9c0994849ae48b9256211a6c5895a614a96147
Sagun Raj Lage <sagsraj@yahoo.com>
2018-03-25 21:30:21 +0545

    Implemented methods like onSaveInstanceState and onRestoreInstanceState to save data when rotation kills an activity and loses data of the selected league and skill

--- app/src/main/java/com/example/sagunraj/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/example/sagunraj/swoosh/Controller/LeagueActivity.kt
@@ -2,6 +2,7 @@ package com.example.sagunraj.swoosh.Controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.example.sagunraj.swoosh.Model.Player
@@ -13,9 +14,21 @@ class LeagueActivity : BaseActivity() {
 
     var player = Player("", "")
 
+    override fun onSaveInstanceState(outState: Bundle?) { //to save the instance data
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player) //can be put at putExtra if no Parcelable is used
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) { //what activity is recreated
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState != null) { //if we have an instance state saved
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
     }
 
     fun leagueNextClicked(view: View){
